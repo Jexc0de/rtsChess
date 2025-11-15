f3602f781ee8ab1d673220ceddb253f96135e364
Jexc0de <jpower28@outlook.com>
2025-11-15 15:46:33 -0500

    setup, added assets for chess, Added a object to share textures between objects

---
+++ build/CMakeFiles/RTSChess.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RTSChess.dir/src/game.cpp.o"
+  "CMakeFiles/RTSChess.dir/src/game.cpp.o.d"
+  "CMakeFiles/RTSChess.dir/src/main.cpp.o"
+  "CMakeFiles/RTSChess.dir/src/main.cpp.o.d"
+  "RTSChess"
+  "RTSChess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RTSChess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
